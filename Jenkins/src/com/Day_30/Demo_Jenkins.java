637460b980d87e914291d4716403dae42f2f751c
anike <anike@Aniket>
2022-12-29 15:51:08 +0530

    by Aniket

---
+++ Jenkins/src/com/Day_30/Demo_Jenkins.java
@@ -0,0 +1,8 @@
+package com.Day_30;
+
+public class Demo_Jenkins {
+	public static void main(String[] args) {
+		System.out.println("Hello Jenkins");
+	}
+
+}
---
+++ Jenkins/src/com/Day_30/Github.java
@@ -0,0 +1,12 @@
+package com.Day_30;
+
+public class Github {
+	public void m1()
+	{
+		System.out.println("Hello m1");
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello github jenkins");
+	}
+
+}
